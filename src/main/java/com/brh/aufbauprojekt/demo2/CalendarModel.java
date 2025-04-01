f3d2928071679704087cfec3f60b544029df70be
airetaM <roth82@gmx.net>
2025-04-01 14:37:56 +0200

    Only today will be marked with a red box now when new dates are chosen with the datepicker

--- src/main/java/com/brh/aufbauprojekt/demo2/CalendarModel.java
+++ src/main/java/com/brh/aufbauprojekt/demo2/CalendarModel.java
@@ -2,6 +2,43 @@ package com.brh.aufbauprojekt.demo2;
 
 public class CalendarModel {
     public static String profileName;
+    public static int currentWeekNumber;
+
+    public static int getCurrentWeekNumber() {
+        return currentWeekNumber;
+    }
+
+    public static void setCurrentWeekNumber(int currentWeekNumber) {
+        CalendarModel.currentWeekNumber = currentWeekNumber;
+    }
+
+    public static int getCurrentDay() {
+        return currentDay;
+    }
+
+    public static void setCurrentDay(int currentDay) {
+        CalendarModel.currentDay = currentDay;
+    }
+
+    public static int getCurrentMonth() {
+        return currentMonth;
+    }
+
+    public static void setCurrentMonth(int currentMonth) {
+        CalendarModel.currentMonth = currentMonth;
+    }
+
+    public static int getCurrentYear() {
+        return currentYear;
+    }
+
+    public static void setCurrentYear(int currentYear) {
+        CalendarModel.currentYear = currentYear;
+    }
+
+    public static int currentDay;
+    public static int currentMonth;
+    public static int currentYear;
 
     public static String getProfileName() {
         return profileName;
@@ -10,4 +47,45 @@ public class CalendarModel {
     public static void setProfileName(String profileName) {
         CalendarModel.profileName = profileName;
     }
+
+    public static int getChosenWeekNumber() {
+        return chosenWeekNumber;
+    }
+
+    public static void setChosenWeekNumber(int chosenWeekNumber) {
+        CalendarModel.chosenWeekNumber = chosenWeekNumber;
+    }
+
+    public static int getChosenDay() {
+        return chosenDay;
+    }
+
+    public static void setChosenDay(int chosenDay) {
+        CalendarModel.chosenDay = chosenDay;
+    }
+
+    public static int getChosenMonth() {
+        return chosenMonth;
+    }
+
+    public static void setChosenMonth(int chosenMonth) {
+        CalendarModel.chosenMonth = chosenMonth;
+    }
+
+    public static int getChosenYear() {
+        return chosenYear;
+    }
+
+    public static void setChosenYear(int chosenYear) {
+        CalendarModel.chosenYear = chosenYear;
+    }
+
+    public static int chosenWeekNumber;
+    public static int chosenDay;
+    public static int chosenMonth;
+    public static int chosenYear;
+
+
+
 }
+
--- src/main/java/com/brh/aufbauprojekt/demo2/MainController.java
+++ src/main/java/com/brh/aufbauprojekt/demo2/MainController.java
@@ -151,11 +151,31 @@ public class MainController {
     @FXML
     private Label clockID;
 
+    private boolean isBlack;
+
     @FXML
     public void initialize() {
         DatePicker.getEditor().setText(date.toString());
+        saveToday();
         confirmDateFromText();
         startClock();
+    }
+
+    protected void saveToday() {
+        LocalDate selectedDate = LocalDate.now();
+            WeekFields weekFields = WeekFields.of(Locale.getDefault());
+            int currentWeekNumber = selectedDate.get(weekFields.weekOfWeekBasedYear());
+            int currentDay = LocalDate.now().getDayOfMonth();
+            int currentMonth = selectedDate.getMonthValue();
+            int currentYear = selectedDate.getYear();
+            CalendarModel.setCurrentWeekNumber(currentWeekNumber);
+            CalendarModel.setCurrentDay(currentDay);
+            CalendarModel.setCurrentMonth(currentMonth);
+            CalendarModel.setCurrentYear(currentYear);
+            CalendarModel.setChosenWeekNumber(currentWeekNumber);
+            CalendarModel.setChosenDay(currentDay);
+            CalendarModel.setChosenMonth(currentMonth);
+            CalendarModel.setChosenYear(currentYear);
     }
 
     @FXML
@@ -168,9 +188,7 @@ public class MainController {
     }
 
     private void updateLabels(LocalDate selectedDate) {
-
         String profileName = CalendarModel.getProfileName();
-
         profileLabel.setText(profileName);
         WeekFields weekFields = WeekFields.of(Locale.getDefault());
         int weekNumber = selectedDate.get(weekFields.weekOfWeekBasedYear());
@@ -179,7 +197,7 @@ public class MainController {
         updateMonth(Month);
         kwField.setText("KW " + weekNumber);
         yearField.setText("" + selectedDate.getYear());
-        setStyles(day);
+        checkStyles(day);
         setDayNumbers(selectedDate, day);
     }
 
@@ -203,15 +221,20 @@ public class MainController {
             int weekNumber = selectedDate.get(weekFields.weekOfWeekBasedYear());
             int day = selectedDate.get(weekFields.dayOfWeek());
             int Month = selectedDate.getMonthValue();
-
+            int currentYear = selectedDate.getYear();
             //todo: datemodel
+            CalendarModel.setChosenWeekNumber(weekNumber);
+            CalendarModel.setChosenDay(day);
+            CalendarModel.setChosenMonth(Month);
+            CalendarModel.setChosenYear(currentYear);
 
-            System.out.println("Selected date: " + selectedDate + "\nWeek number: " + weekNumber);
+            System.out.println("woche: " + weekNumber + " tag: " + day + " monat: " + Month + " jahr: " + currentYear);
             kwField.setText("KW " + weekNumber);
             updateMonth(Month);
             yearField.setText("" + selectedDate.getYear());
 //                todayField.setText("Monat: " + selectedDate.getMonthValue() + " Tag: " + selectedDate.getDayOfMonth());
             setDayNumbers(selectedDate, day);
+            checkStyles(day);
         }
     }
 
@@ -232,23 +255,47 @@ public class MainController {
 
     }
 
-    private void setStyles(int day) {
-        String numberStyle = "-fx-font-size:22; -fx-text-fill: red; -fx-font-weight: bold; -fx-font-family: 'Comic Sans MS', sans-serif;";
-        String boxStyle = "-fx-border-width: 5px; -fx-border-color: red; -fx-background-color: #ffe6e6;";
-        String blackNumberStyle = "-fx-font-size:18; -fx-text-fill: black; -fx-font-weight: normal; -fx-font-family: 'Comic Sans MS', sans-serif;";
-        String blackBoxStyle = "-fx-border-width: 2px; -fx-border-color: black; -fx-background-color: #acfcef;";
-        Label[] dayNumbers = {dayNumber1, dayNumber2, dayNumber3, dayNumber4, dayNumber5, dayNumber6, dayNumber7};
-        Label[] dayIDs = {dayID1, dayID2, dayID3, dayID4, dayID5, dayID6, dayID7};
-        for (int i = 0; i < dayNumbers.length; i++) {
-            if (day == i + 1) {
-                dayNumbers[i].setStyle(numberStyle);
-                dayIDs[i].setStyle(numberStyle + boxStyle);
+    private void checkStyles(int day) {
+        if (LocalDate.now().getYear() == CalendarModel.getCurrentYear()) {
+            if (CalendarModel.getChosenWeekNumber() == CalendarModel.getCurrentWeekNumber()) {
+                System.out.println(""+LocalDate.now().getDayOfYear());
+                System.out.println("current:"+CalendarModel.getCurrentDay());
+                System.out.println("chosen"+CalendarModel.getChosenDay());
+                // LocalDate.now().getDayOfWeek().getValue()
+               if (day == CalendarModel.getCurrentDay() + 1 || isBlack) {
+                    isBlack = false;
+                    String numberStyle = "-fx-font-size:22; -fx-text-fill: red; -fx-font-weight: bold; -fx-font-family: 'Comic Sans MS', sans-serif;";
+                    String boxStyle = "-fx-border-width: 5px; -fx-border-color: red; -fx-background-color: #ffe6e6;";
+                    String blackNumberStyle = "-fx-font-size:18; -fx-text-fill: black; -fx-font-weight: normal; -fx-font-family: 'Comic Sans MS', sans-serif;";
+                    String blackBoxStyle = "-fx-border-width: 2px; -fx-border-color: black; -fx-background-color: #acfcef;";
+                    day = CalendarModel.getCurrentDay() + 1;
+                    setStyles(day, numberStyle, boxStyle, blackNumberStyle, blackBoxStyle);
+              }
             } else {
-                dayNumbers[i].setStyle(blackNumberStyle);
-                dayIDs[i].setStyle(blackNumberStyle + blackBoxStyle);
+                isBlack = true;
+                String numberStyle = "-fx-font-size:18; -fx-text-fill: black; -fx-font-weight: normal; -fx-font-family: 'Comic Sans MS', sans-serif;";
+                String boxStyle = "-fx-border-width: 2px; -fx-border-color: black; -fx-background-color: #acfcef;";
+                String blackNumberStyle = "-fx-font-size:18; -fx-text-fill: black; -fx-font-weight: normal; -fx-font-family: 'Comic Sans MS', sans-serif;";
+                String blackBoxStyle = "-fx-border-width: 2px; -fx-border-color: black; -fx-background-color: #acfcef;";
+                setStyles(day, numberStyle, boxStyle, blackNumberStyle, blackBoxStyle);
             }
         }
     }
+        private void setStyles(int day, String numberStyle, String boxStyle, String blackNumberStyle, String blackBoxStyle) {
+
+                Label[] dayNumbers = {dayNumber1, dayNumber2, dayNumber3, dayNumber4, dayNumber5, dayNumber6, dayNumber7};
+                Label[] dayIDs = {dayID1, dayID2, dayID3, dayID4, dayID5, dayID6, dayID7};
+                for (int i = 0; i < dayNumbers.length; i++) {
+
+                    if (day == i + 1) {
+                        dayNumbers[i].setStyle(numberStyle);
+                        dayIDs[i].setStyle(numberStyle + boxStyle);
+                    } else {
+                        dayNumbers[i].setStyle(blackNumberStyle);
+                        dayIDs[i].setStyle(blackNumberStyle + blackBoxStyle);
+                    }
+                }
+            }
 
     private void startClock() {
         Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
